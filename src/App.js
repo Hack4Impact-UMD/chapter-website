93b017bf77ae42612c6e823e1dd9c5da3da14d9f
Abbie Tran <abbie.y.tran99@gmail.com>
2020-10-11 01:45:58 -0400

    Initial create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Hack4Impact-UMD </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
