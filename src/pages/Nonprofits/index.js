f161c246ba15d73c76bd5f3b2215680454852479
Abbie Tran <abbie.y.tran99@gmail.com>
2020-10-17 21:35:00 -0400

    Created framework for website

---
+++ src/pages/Nonprofits/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Nonprofits = () => {
+    return (
+        <div>
+            <h1>
+                Nonprofits
+            </h1>
+        </div>
+    )
+}
+export default Nonprofits;
