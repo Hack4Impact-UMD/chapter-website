50ed7b72a47eae91b021bf8ad354bf0ac51cf5aa
toastymcvoid <andyqu10@gmail.com>
2020-10-21 17:22:49 -0400

    Got started, don't really know what to do

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,6 +7,9 @@ const Home = () => {
     return (
         <div>
             <h1>Hack4Impact UMD</h1>
+            <p>We are a student-run 501(c)(3) organization that empowers engineers, designers, activists, and humanitarians to create lasting social change by developing projects for local nonprofits. We foster the wider adoption of software as a tool for social good</p>
+            <h2>Our Projects</h2>
+            {/** I'm not quite sure what to put here */}
   
         </div>
     );
