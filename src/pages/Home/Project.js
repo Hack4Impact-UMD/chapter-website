f06553b973e42b9512c0670661b2818a6ddae890
toastymcvoid <andyqu10@gmail.com>
2020-10-22 16:02:40 -0400

    Cannot continue past this point, Module not found: Can't resolve 'react-boostrap' in '...\chapter-website\src\pages\Home'

--- src/pages/Home/Project.js
+++ src/pages/Home/Project.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Card } from 'react-boostrap'
+import { ListGroup } from 'react-bootstrap'
+import { ListGroupItem } from 'react-bootstrap'
 
 /**
  * This Project thingy displays a project (idk correct terminology)
@@ -6,13 +9,38 @@ import React from 'react'
  */
 
 const Project = props => {
+    
     return (
+        <>
+            <Card style={{width: 'auto', background: '#f4f4f4' }}>
+                <img src="" style={{width: '100%'}} />
+                <Card.Img varient='top' src="" />
+                <Card.body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>{props.description}</Card.Text>
+                    <Card.Title>Members</Card.Title>
+
+                    {/* maps through the list of members and creates an li element for each one */}
+                    <ListGroup variant='flush'>
+                        {props.members.map(member => {
+                            return (
+                                <ListGroupItem>{member}</ListGroupItem>
+                            )
+                        })}
+                    </ListGroup>
+                </Card.body>
+            </Card>
+        </>
+    )
+
+    /* return (
         <div>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <h2>Members</h2>
             <ol>
-                {/* maps through the list of members and creates an li element for each one */}
+                {// maps through the list of members and creates an li element for each one
+                }
                 {props.members.map(member => {
                     return (
                         <li>{member}</li>
@@ -20,7 +48,7 @@ const Project = props => {
                 })}
             </ol>
         </div>
-    )
+    ) */
 }
 
 export default Project;
