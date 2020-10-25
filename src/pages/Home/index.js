30eaff8e063baf4dc1e670ba44543a725f45212e
Abbie Tran <abbie.y.tran99@gmail.com>
2020-10-25 00:04:26 -0400

    Centered page and aligned projects

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Project from './Project'
+import Project from './Project';
+import {Container, Card, Button, Row, Col} from "react-bootstrap";
+import styles from "./Cardgrid.module.css";
 
 /**
  * Array of project objects
@@ -25,23 +27,42 @@ const projs = [
  */
 const Home = () => {
     return (
-        <div>
-            <h1>Hack4Impact UMD</h1>
-            <p>Hack4Impact-UMD is a group of University of Maryland students who work with local and national non-profits to create software that improves the world around us. We also dicuss ethical technology, have talks about using technology for social good, and have fun! This club was founded in 2020 and is a chapter of the Hack4Impact, a national 501(c)3 organization.</p>
-            <h1>Our Projects</h1>
+        <Container style = {{marginLeft: '4em', marginRight: '4em'}}>
+        <h1>Hack4Impact UMD</h1>
+        <p>Hack4Impact-UMD is a group of University of Maryland students who work with local and national non-profits to create software that improves the world around us. We also dicuss ethical technology, have talks about using technology for social good, and have fun! This club was founded in 2020 and is a chapter of the Hack4Impact, a national 501(c)3 organization.</p>
+        {/* Project Cards*/}
+        <Card style={{ marginTop: '4em',marginBottom: '4em',alignItems: 'center',textAlign: 'center', background: 'aliceblue' }}>
+            <Card.Body>
+            <Container>
+                <Row>
+                    <Col style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                      <h3 style={{ marginBottom:'1em' }}>Our Projects</h3>
+                    </Col>
+                </Row>
+              </Container>
+                <Row>
+                <Container className = {styles.mosaic}>
+           
 
-            {/* maps through the projects and returns a Project element to display, passing in attributes of the project */}
-            {projs.map(proj => {
-                return (
-                    <Project 
-                        title_img = {proj.title_img}
-                        description = {proj.description}
-                        date = {proj.date}
-                        members = {proj.members}
-                    />
-                )
-            })}  
-        </div>
+           {/* maps through the projects and returns a Project element to display, passing in attributes of the project */}
+           {projs.map(proj => {
+               return (
+                   <Project 
+                       title_img = {proj.title_img}
+                       description = {proj.description}
+                       date = {proj.date}
+                       members = {proj.members}
+                   />
+               )
+           })}  
+       </Container>
+                </Row>
+                <Button style={{marginTop: '1em'}} variant="solid" href="../Projects">Learn More</Button>
+                
+              </Card.Body>
+        </Card>
+        
+        </Container>
     );
 }
 
