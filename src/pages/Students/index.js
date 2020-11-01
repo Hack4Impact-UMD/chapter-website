df70fc183156bc552c684eb7d3c188dff0aa5886
Ferzam Mohammad <ferzammohammad99@gmail.com>
2020-11-01 01:35:08 -0500

    Added a couple of real questions to FAQ

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -21,6 +21,19 @@ const timelineItems = [
         description: "hello testing"
     }
 ]
+
+const FAQAnswers = [
+    "We recruit for software developers, product managers, and designers. On each nonprofit " +
+    "project team, we have around 4-5 developers, 1-2 product managers, and 1-2 designers. If there " +
+    "is a role that we don't currently offer and that interests you, let us know! We want our members " +
+    "to be passionate about their work and are always looking to make our project teams stronger.",
+
+    "Monthly GBMs for tech + social good talks, ethical software discussions, project demos, and more fun (1 hour per month)\n" + 
+    "Weekly meetings with your project team (1 hour per week)\n" +
+    "Work on the projects in your own time (2 to 3 hours)\n" +
+    "Placed on project teams with nonprofit OR bootcamp team, depending on experience\n" +
+    "Leadership roles, such as executive directors, project managers, and tech leads will spend more time, up to 10 hours a week."
+]
 class Students extends React.Component{
 
     constructor(props) {
@@ -52,8 +65,8 @@ class Students extends React.Component{
                         <br/>
                         <h1>Frequently Asked Questions</h1>
                         <div class="faq-list">
-                            <FAQItem question = "Question 1" answer = "Answer 1" />
-                            <FAQItem question = "Question 2" answer = "Answer 2" />
+                            <FAQItem question = "What types of roles can students have?" answer = {FAQAnswers[0]} />
+                            <FAQItem question = "How much of a time commitment is Hack4Impact?" answer = {FAQAnswers[1]} />
                         </div>
                     </section>
                 </div>
