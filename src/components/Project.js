f029fb1e3ffb8059d12c18fdeb85ff2e5f2eceae
Abbie Tran <abbie.y.tran99@gmail.com>
2020-11-14 15:21:58 -0500

    Changed styling on project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,8 +7,27 @@ class ProjectCard extends React.Component {
     constructor(props) {
         super(props)
     }
-
+    
   render() {
+
+    // Information about members should only appear on the projects page
+    let members = <></>;
+    if (this.props.members !== undefined) {
+        members = <Card.Text style={{marginBottom: '.3rem'}}>
+        <b>Team Members: </b>
+        {this.props.members}
+    </Card.Text>;
+    }
+
+    // GitHub Icon only appears on the projects page
+    let githubIcon = <></>;
+    if (this.props.github !== undefined) {
+        githubIcon = 
+        <a style={{marginRight: '5px'}} href={this.props.github}>
+            <FaGithub  style={{width: '30px', height: '30px', color: 'black'}} />
+        </a>;
+    }
+
     return (
         <div className={styles.card}>
             <Card style={{overflow: 'auto', marginBottom: '30px', width: '100%', height: '100%'}} >
@@ -18,20 +37,16 @@ class ProjectCard extends React.Component {
                 </div>
 
                 <Card.Body>
+                    <Card.Subtitle className="mb-2 text-muted">{this.props.date}</Card.Subtitle>
                     <Card.Text style={{marginBottom: '.3rem'}}>
                         {this.props.description}
                     </Card.Text>
 
-                    <Card.Text style={{marginBottom: '.3rem'}}>
-                        <b>Team Members: </b>
-                        {this.props.members}
-                    </Card.Text>
+                    {members}                    
 
                     <div style={{margin: '10px'}}/>
                     <div style={{alignContent: 'center', alignItems: 'center', textAlign: 'center'}}> 
-                        <a style={{marginRight: '5px'}} href={this.props.github}>
-                            <FaGithub  style={{width: '30px', height: '30px', color: 'black'}} />
-                        </a>
+                       {githubIcon}
                     </div>
                     
                 </Card.Body>
